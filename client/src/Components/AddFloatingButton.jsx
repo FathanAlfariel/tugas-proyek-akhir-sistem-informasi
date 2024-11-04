9ace55956c20eaa4e2f9e1fd4c844222111531e3
FathanAlfariel <faril191204@gmail.com>
2024-11-04 08:42:00 +0700

    Added floating button and added border in user's menu

--- client/src/Components/AddFloatingButton.jsx
+++ client/src/Components/AddFloatingButton.jsx
@@ -1,31 +1,81 @@
-import Button from "./Button";
+import { useEffect, useState } from "react";
+import { LuPlus } from "react-icons/lu";
+import { Link } from "react-router-dom";
 
 const AddFloatingButton = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleShowMenu = () => {
+    setShowMenu((prev) => !prev);
+  };
+
+  // Closes the menu when the user clicks outside the div
+  useEffect(() => {
+    if (showMenu) {
+      window.addEventListener("click", (e) => {
+        if (
+          !document.getElementById("floating-button-menu").contains(e.target)
+        ) {
+          setShowMenu(false);
+        }
+      });
+    }
+  }, [showMenu]);
+
   return (
     <>
       {/* Add Floating Button  */}
       <div className="fixed bottom-20 right-20">
-        <Button className="pl-4 pr-5 py-4 flex items-center gap-x-3 rounded-2xl text-sm font-medium bg-blue-300 shadow-md hover:shadow-lg">
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-plus"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M12 5l0 14" />
-              <path d="M5 12l14 0" />
-            </svg>
-          </span>
-          Tambah
-        </Button>
+        <div id="floating-button-menu" className="relative">
+          <button
+            onClick={handleShowMenu}
+            className="group bg-[#EADDFF] text-[#21005D] text-sm font-medium rounded-2xl overflow-hidden transition-all active:scale-90 duration-300 shadow-lg hover:shadow-xl"
+          >
+            <div className="flex items-center gap-x-3 pl-4 pr-5 py-4 transition-all duration-300 group-hover:bg-[#21005D]/[.08]">
+              <span>
+                <LuPlus className="text-2xl" />
+              </span>
+              Tambah
+            </div>
+          </button>
+
+          {showMenu && (
+            <div className="min-w-56 absolute bottom-full right-0 mb-1 rounded-xl bg-white shadow-md py-2.5 border border-[#F1F1F1]">
+              <ul>
+                <li>
+                  <Link to={"/admin/product/add"}>
+                    <button
+                      onClick={() => setShowMenu(false)}
+                      className="w-full pl-4 pr-6 py-2 text-left text-sm hover:bg-[#1D1B20]/[.08]"
+                    >
+                      Tambah produk
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <button className="w-full pl-4 pr-6 py-2 text-left text-sm hover:bg-[#1D1B20]/[.08]">
+                    Tambah pesanan
+                  </button>
+                </li>
+                <li>
+                  <button className="w-full pl-4 pr-6 py-2 text-left text-sm hover:bg-[#1D1B20]/[.08]">
+                    Tambah pengeluaran
+                  </button>
+                </li>
+                <li>
+                  <button className="w-full pl-4 pr-6 py-2 text-left text-sm hover:bg-[#1D1B20]/[.08]">
+                    Tambah penjahit
+                  </button>
+                </li>
+                <li>
+                  <button className="w-full pl-4 pr-6 py-2 text-left text-sm hover:bg-[#1D1B20]/[.08]">
+                    Tambah pembuatan produk
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
