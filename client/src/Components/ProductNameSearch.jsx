301740f38ae374f4175a044da4547471dc3618eb
FathanAlfariel <faril191204@gmail.com>
2024-12-02 19:31:24 +0700

    Added suggestions for search by product name

--- client/src/Components/ProductNameSearch.jsx
+++ client/src/Components/ProductNameSearch.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 
 const ProductNameSearch = () => {
   const parentRef = useRef(null);
@@ -8,6 +9,13 @@ const ProductNameSearch = () => {
   const [isFocused, setIsFocused] = useState(false);
 
   const [products, setIsProducts] = useState([]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentParams = Object.firstEntries(searchParams.entries());
+
+  const [productName, setProductName] = useState(
+    currentParams?.productName || ""
+  );
 
   // Get all products
   const fetchProducts = async () => {
@@ -96,6 +104,41 @@ const ProductNameSearch = () => {
                   <div className="w-full h-px bg-[#CAC4D0] my-3"></div>
                 </>
               )}
+
+              <ul>
+                {products?.map((product, key) => {
+                  return (
+                    <li key={key}>
+                      <Link to={`/products/${product?.id}`}>
+                        <button className="flex items-center gap-x-4 w-full text-left text-sm pl-4 pr-6 py-2 hover:bg-[#1D1B20]/[.08]">
+                          <img
+                            src={`http://localhost:5000/public/images/${product?.images[0]?.name}`}
+                            alt={product?.images[0]?.name}
+                            className="w-12 h-12 rounded-lg object-cover"
+                          />
+
+                          <div className="flex flex-col gap-y-0.5">
+                            <p className="text-sm line-clamp-1">
+                              {product.name}
+                            </p>
+                            <p className="text-sm text-[#6A6A6A] line-clamp-1">
+                              Warna:{" "}
+                              {product?.variants?.map((item, key) => (
+                                <span key={key}>
+                                  {item.color}
+                                  {key !== product.variants.length - 1
+                                    ? ", "
+                                    : ""}
+                                </span>
+                              ))}
+                            </p>
+                          </div>
+                        </button>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
           </div>
         )}
